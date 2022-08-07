f7528eb415af268867ec1558d17fa109c98b5bb5
鲍珍龙 <435801541@qq.com>
2022-08-07 18:05:43 +0800

    feat: attack anim

--- Assets/Scripts/CharacterCombat.cs
+++ Assets/Scripts/CharacterCombat.cs
@@ -9,6 +9,9 @@ public class CharacterCombat : MonoBehaviour
     private float attackCountdown = 0f;
     public float attackRate = 1f;
     public event System.Action OnAttack = null;
+    public bool InCombat { get; private set; }
+    public float combatCoolingTime = 5f;
+    private float lastAttackTime = .0f;
     void Start()
     {
         myStats = GetComponent<CharacterStats>();
@@ -17,6 +20,10 @@ public class CharacterCombat : MonoBehaviour
     void Update()
     {
         attackCountdown -= Time.deltaTime;
+        if (Time.time - lastAttackTime > combatCoolingTime)
+        {
+            InCombat = false;
+        }
     }
 
     public void Attack(CharacterStats enemyStats)
@@ -29,6 +36,8 @@ public class CharacterCombat : MonoBehaviour
             }
             StartCoroutine(DoDamage(enemyStats, .6f));
             attackCountdown = 1f / attackRate;
+            InCombat = true;
+            lastAttackTime = Time.time;
         }
     }
     IEnumerator DoDamage(CharacterStats stats, float delay)
@@ -36,5 +45,9 @@ public class CharacterCombat : MonoBehaviour
         yield return new WaitForSeconds(delay);
         stats.TakeDamage(myStats.damage.GetValue());
         Debug.Log(transform.name + " attack and " + stats.gameObject.name + " takes " + myStats.damage.GetValue() + " damage");
+        if (stats.currentHealth <= 0)
+        {
+            InCombat = false;
+        }
     }
 }
--- Assets/Scripts/Controller/PlayerAnimator.cs
+++ Assets/Scripts/Controller/PlayerAnimator.cs
@@ -9,15 +9,32 @@ public class PlayerAnimator : MonoBehaviour
     private Animator animator;
     private float speedPercent;
     public float locomotionSmoothTime = .1f;
+    private CharacterCombat combat;
+    private AnimatorOverrideController animatorOverrideController;
+    public AnimationClip[] attackAnimationClips;
+    public AnimationClip attackPlaceholderAnimClip;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
+        combat = GetComponent<CharacterCombat>();
+        combat.OnAttack += OnAttack;
+        animatorOverrideController = new AnimatorOverrideController(animator.runtimeAnimatorController);
+        animator.runtimeAnimatorController = animatorOverrideController;
     }
 
     void Update()
     {
         speedPercent = agent.velocity.magnitude / agent.speed;
         animator.SetFloat("speedPercent", speedPercent, locomotionSmoothTime, Time.deltaTime);
+        animator.SetBool("inCombat", combat.InCombat);
+    }
+
+    void OnAttack()
+    {
+        animator.SetTrigger("attack");
+        int attackIndex = Random.Range(0, attackAnimationClips.Length);
+        Debug.Log("Attack name: " + attackAnimationClips[attackIndex].name);
+        animatorOverrideController[attackPlaceholderAnimClip.name] = attackAnimationClips[attackIndex];
     }
 }
